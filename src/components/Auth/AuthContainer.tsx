f1446f5b11403e8582a11e363869319583b04db6
Yass94230 <y.hannach@yhconsulting.fr>
2025-05-10 21:10:10 +0200

    Updated AuthContainer.tsx

--- src/components/Auth/AuthContainer.tsx
+++ src/components/Auth/AuthContainer.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
-import { useNavigate } from 'react-router-dom';
 
 interface AuthContainerProps {
   onSuccess: () => void;
@@ -9,20 +8,11 @@ interface AuthContainerProps {
 
 const AuthContainer: React.FC<AuthContainerProps> = ({ onSuccess }) => {
   const [showRegister, setShowRegister] = useState(false);
-  const navigate = useNavigate();
 
   // Fonction qui sera appelée après une connexion ou inscription réussie
   const handleAuthSuccess = () => {
     console.log("Authentification réussie dans AuthContainer");
     onSuccess();
-    
-    // Vérifier si l'utilisateur a déjà configuré WhatsApp
-    const phoneNumber = localStorage.getItem('userWhatsAppNumber');
-    const isVerified = localStorage.getItem('whatsapp_verified') === 'true';
-    
-    if (phoneNumber && isVerified) {
-      navigate('/admin');
-    }
   };
 
   return showRegister ? (
