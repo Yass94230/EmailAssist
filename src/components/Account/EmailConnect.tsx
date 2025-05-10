38bb1f9ebeca8c3590533302233006c10e718f10
Yass94230 <y.hannach@yhconsulting.fr>
2025-05-10 16:22:19 +0200

    Add default export to EmailConnect component

--- src/components/Account/EmailConnect.tsx
+++ src/components/Account/EmailConnect.tsx
@@ -1,6 +1,20 @@
-// Mise à jour du composant de chargement
-{isLoading && (
-  <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-    <Spinner size="lg" className="text-green-500" />
-  </div>
-)}
+import React from 'react';
+import { Spinner } from '../ui/Spinner';
+
+interface EmailConnectProps {
+  isLoading?: boolean;
+}
+
+const EmailConnect: React.FC<EmailConnectProps> = ({ isLoading = false }) => {
+  return (
+    <>
+      {isLoading && (
+        <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+          <Spinner size="lg" className="text-green-500" />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default EmailConnect;
