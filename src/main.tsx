eecaaec398ce11d68c2c61deef4d80e08af658c4
Yass94230 <y.hannach@yhconsulting.fr>
2025-05-10 17:02:49 +0200

    Remove duplicate router and fix routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx';
 import './index.css';
@@ -13,10 +12,8 @@ if (!GOOGLE_CLIENT_ID) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-        <App />
-      </GoogleOAuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
